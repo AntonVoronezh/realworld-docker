69078b513a08980352f36615883bae4e143661e4
Anton <glushenkovai@it2g.ru>
2021-12-21 18:39:36 +0300

    mongo

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const {connectDB} = require("./helpers/db");
 const {port, host, db} = require("./configuration");
+const mongoose = require("mongoose");
 
 const app = express();
 
+const postSchema = new mongoose.Schema({
+    name: String
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 app.get('/test', (req, res) => {
     res.send('Our api server is working correctly')
@@ -14,6 +20,27 @@ const startServer = () => {
         console.log(`Started api service on port ${port}`)
         console.log(`Started api service on host ${host}`)
         console.log(`Our database ${db}`)
+
+        const silence = new Post({ name: 'Silence' });
+        // console.log(silence); // 'Silence'
+        silence.save((err, savedSilence)=>{
+            if(err) {
+                console.log(err)
+                return
+            }
+
+            console.log('savedSilence', savedSilence)
+        }
+)
+
+        // Post.find((err, posts)=>{
+        //     if(err) {
+        //         console.log(err)
+        //         return
+        //     }
+        //
+        //     console.log('Posts', posts)
+        // })
     })
 }
 
