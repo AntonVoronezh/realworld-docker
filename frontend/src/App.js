286b65a12e31e3b60e78749d2da459c4c44dd39e
Anton <glushenkovai@it2g.ru>
2021-12-22 18:29:06 +0300

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit5 <code>src/App.js</code> and save to reload 77.
+          Edit5 <code>src/App.js</code> and save to reload 7997.
         </p>
         <a
           className="App-link"
